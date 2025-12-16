2586d63aeed2a1115912012299f9145b04191a81
Marco Santos <marco@sociap.com.br>
2025-12-15 21:30:10 -0300

    voting

--- cardano/connect.js
+++ cardano/connect.js
@@ -1,4 +1,3 @@
-// import format from "path";
 
 function isNullOrEmptyOrUndefined(value) {
     return value == null || value === "" || (Array.isArray(value) && value.length === 0);
@@ -38,34 +37,38 @@ function removeCookie(k, path) {
 }
 
 function hexToBytes(hex) {
-    for (var bytes = [], c = 0; c < hex.length; c += 2){
-      bytes.push(parseInt(hex.substr(c, 2), 16));
+    for (var bytes = [], c = 0; c < hex.length; c += 2) {
+        bytes.push(parseInt(hex.substr(c, 2), 16));
     }
     return bytes;
 }
 
 function bytesToHex(bytes) {
-   return Array.from(bytes)
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('');
+    return Array.from(bytes)
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('');
 }
 
 async function getPints(carteira) {
     if (!window.cardano) {
-      return { "err" : "nowallet"};
+        return { "err": "nowallet" };
     }
 
-    const wasm = await import("https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@9.1.2/cardano_serialization_lib.min.js");
+    //    const wasm = await import("https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@9.1.2/cardano_serialization_lib.min.js");
+    const wasm = await import("https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@15.0.3/cardano_serialization_lib.min.js");
 
     switch (carteira) {
         case "eternl":
-            api = await window.cardano.eternl.enable({'extensions': [{'cip':30}]});
+            api = await window.cardano.eternl.enable({ 'extensions': [{ 'cip': 30 }] });
+            break;
+        case "nufi":
+            api = await window.cardano.nufi.enable({ 'extensions': [{ 'cip': 30 }] });
             break;
         case "nami":
-            api = await window.cardano.nami.enable({'extensions': [{'cip':30}]});
+            api = await window.cardano.nami.enable({ 'extensions': [{ 'cip': 30 }] });
             break;
         case "lace":
-            api = await window.cardano.lace.enable({'extensions': [{'cip':30}]});
+            api = await window.cardano.lace.enable({ 'extensions': [{ 'cip': 30 }] });
             break;
         default:
             throw new Error(`Wallet not supported : ${carteira}`);
@@ -78,15 +81,15 @@ async function getPints(carteira) {
     const lovelaces = balance.coin().to_str();
     const multiAsset = balance.multiasset();
     if (!multiAsset) {
-      console.log("Sem multiassets.");
-      return { "small" : createSmall(address), "address" : address, "ada" : lovelaces, "pint" : 0};
+        console.log("Sem multiassets.");
+        return { "small": createSmall(address), "address": address, "ada": lovelaces, "pool": 0 };
     }
 
     const policies = multiAsset.keys();
     for (let i = 0; i < policies.len(); i++) {
         const policyIdBytes = policies.get(i);
         const policyId = bytesToHex(policyIdBytes.to_bytes());
-        if (policyId == "7e7dbfe44b4145a2f994a9c649ce84a3ee0706f06e9ec82360e0f642"){
+        if (policyId == "7e7dbfe44b4145a2f994a9c649ce84a3ee0706f06e9ec82360e0f642") {
             const listAssets = multiAsset.get(policyIdBytes);
             for (let j = 0; j < listAssets.len(); j++) {
                 const assetNameBytes = listAssets.keys();
@@ -95,16 +98,19 @@ async function getPints(carteira) {
                     const assetName = bytesToHex(assetNameBin.name());
                     if (assetName == "50494e54") {
                         const quantity = listAssets.get(assetNameBin).to_str(); // string
-                        return { "small" : createSmall(address), "address" : address, "ada" : lovelaces, "pint" : quantity};
+                        //                        if (quantity == "undefined") {
+                        //                           quantity = 0
+                        //                        }
+                        return { "small": createSmall(address), "address": address, "ada": lovelaces, "pint": quantity };
                     }
                 };
             }
         }
     }
-    return { "small" : createSmall(address), "address" : address, "ada" : lovelaces, "pint" : 0};
+    return { "small": createSmall(address), "address": address, "ada": lovelaces, "pint": 0 };
 }
 
-function createSmall(address){
+function createSmall(address) {
     if (!address || address.length < 8) {
         return address;
     }
@@ -116,17 +122,17 @@ async function connectWallet() {
     carteira = urlParams.get('w');
     if (isNullOrEmptyOrUndefined(carteira)) {
         carteira = getCookie("cardano");
-    }else{
+    } else {
         var tempodevida = new Date();
         tempodevida.setTime(tempodevida + (100 * 60 * 60 * 24 * 2));
         setCookie("cardano", carteira, tempodevida);
     }
 
-    if (!isNullOrEmptyOrUndefined(carteira)){
+    if (!isNullOrEmptyOrUndefined(carteira)) {
         user = await getPints(carteira);
-        if (!isNullOrEmptyOrUndefined(user.small)){
+        if (!isNullOrEmptyOrUndefined(user.small)) {
             btn = document.getElementById("btn-connect")
-            btn.text = user.small + ' (' + user.pint + ' PINT)'
+            btn.textContent = user.small + ' (' + user.pint + ' PINT)'
             if (user.pint < 100) {
                 return "buy";
             }
